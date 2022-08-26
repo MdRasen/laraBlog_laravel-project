2088dcc242311a050a337fe4774b4b8d1afa4462
Md Al Amin Hossen <aamin.hossen99@gmail.com>
2022-08-26 20:48:15 +0600

    Admin dashboard setup

--- laraBlog/routes/web.php
+++ laraBlog/routes/web.php
@@ -1,6 +1,10 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Auth;
+use SebastianBergmann\CodeCoverage\Report\Html\Dashboard;
+
+use App\Http\Controllers\Admin\dashboardController;
 
 /*
 |--------------------------------------------------------------------------
@@ -15,4 +19,12 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Route::prefix('admin')->group(function () {
+    Route::get('/dashboard', [dashboardController::class,'index']);
 });
